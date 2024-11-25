ac677fdede713ddc8ce3c00172b6408c269b3f1e
Faris <muhammad.farishafizh@student.upj.ac.id>
2024-11-25 14:11:26 +0700

    buat verifikasi email

--- app/src/main/java/com/example/capstonebangkitpawers/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/capstonebangkitpawers/fragment/ProfileFragment.kt
@@ -7,11 +7,14 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.credentials.ClearCredentialStateRequest
 import androidx.credentials.CredentialManager
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.Glide
+import com.example.capstonebangkitpawers.BuildConfig
 import com.example.capstonebangkitpawers.R
 import com.example.capstonebangkitpawers.main.MainViewModel
 import com.example.capstonebangkitpawers.main.ViewModelFactory
@@ -22,17 +25,24 @@ import com.example.capstonebangkitpawers.view.WelcomeActivity
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import de.hdodenhof.circleimageview.CircleImageView
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class ProfileFragment : Fragment() {
 
     private lateinit var mainViewModel: MainViewModel
     private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
 
         mainViewModel = ViewModelProvider(
@@ -40,33 +50,80 @@ class ProfileFragment : Fragment() {
             ViewModelFactory.getInstance(requireContext())
         )[MainViewModel::class.java]
 
+        auth = Firebase.auth
+
+        database = FirebaseDatabase.getInstance(BuildConfig.DATABASE_URL).reference
+
+        setupUser(view)
+        setupLogout(view)
+        setupPengaturan(view)
+        setupRiwayat(view)
+
+        return view
+    }
+
+    private fun setupUser(view: View) {
+        val yourNameTextView: TextView = view.findViewById(R.id.yourName)
+        val profileImageView: CircleImageView = view.findViewById(R.id.profileImage1)
+        val currentUser = auth.currentUser
+
+        if (currentUser != null) {
+            val userRef = database.child("users").child(currentUser.uid)
+
+            userRef.get()
+                .addOnSuccessListener { snapshot ->
+                    val name = snapshot.child("name").getValue(String::class.java) ?: "Nama tidak ditemukan"
+                    val photoUrl = snapshot.child("photoUrl").getValue(String::class.java) ?: "default"
+
+                    // Set nama pengguna
+                    yourNameTextView.text = name
+                    Log.d("ProfileFragment", "Nama pengguna: $name")
+
+                    // Set foto profil
+                    if (photoUrl == "default") {
+                        profileImageView.setImageResource(R.drawable.profile_placeholder)
+                    } else {
+                        Glide.with(this)
+                            .load(photoUrl)
+                            .placeholder(R.drawable.profile_placeholder)
+                            .error(R.drawable.profile_placeholder)
+                            .into(profileImageView)
+                    }
+                }
+                .addOnFailureListener { e ->
+                    Log.e("ProfileFragment", "Gagal mengambil data pengguna: ${e.message}")
+                    yourNameTextView.text = "Gagal memuat nama"
+                    profileImageView.setImageResource(R.drawable.profile_placeholder)
+                }
+        } else {
+            yourNameTextView.text = "Tidak ada pengguna"
+            profileImageView.setImageResource(R.drawable.profile_placeholder)
+        }
+    }
+
+    private fun setupLogout(view: View) {
         val logoutContainer: View = view.findViewById(R.id.logoutContainer)
         logoutContainer.setOnClickListener {
             showLogoutDialog()
         }
+    }
 
-        auth = Firebase.auth
-        val firebaseUser = auth.currentUser
-
-
-
+    private fun setupPengaturan(view: View) {
         val pengaturan: View = view.findViewById(R.id.pengaturanContainer)
         pengaturan.setOnClickListener {
             val intent = Intent(requireContext(), SettingsActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
             startActivity(intent)
         }
+    }
 
-
-
+    private fun setupRiwayat(view: View) {
         val riwayat: View = view.findViewById(R.id.riwayatContainer)
         riwayat.setOnClickListener {
             val intent = Intent(requireContext(), HistoryActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
             startActivity(intent)
         }
-
-        return view
     }
 
     private fun showLogoutDialog() {
@@ -86,20 +143,49 @@ class ProfileFragment : Fragment() {
     private fun logout() {
         lifecycleScope.launch {
             val credentialManager = CredentialManager.create(requireContext())
-            auth.signOut()
-            credentialManager.clearCredentialState(ClearCredentialStateRequest())
+            val currentUser = auth.currentUser
 
-            val intent = Intent(requireContext(), WelcomeActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            startActivity(intent)
+            if (currentUser != null) {
+                val database = FirebaseDatabase.getInstance(databaseURL)
+                val userRef = database.getReference("users").child(currentUser.uid)
+
+                try {
+                    // Perbarui isLogin menjadi false di Realtime Database
+                    userRef.child("isLogin").setValue(false).await()
+                    Log.d("Logout", "isLogin diperbarui menjadi false.")
+                } catch (e: Exception) {
+                    Log.e("Logout", "Gagal memperbarui isLogin: ${e.message}")
+                }
+
+                // Lanjutkan proses logout
+                performLogout(credentialManager)
+            } else {
+                // Jika pengguna tidak ditemukan, langsung logout
+                performLogout(credentialManager)
+            }
         }
     }
 
+    private suspend fun performLogout(credentialManager: CredentialManager) {
+        withContext(Dispatchers.IO) {
+            try {
+                auth.signOut()
+                credentialManager.clearCredentialState(ClearCredentialStateRequest())
 
-//    private fun logout() {
-//        mainViewModel.logout()
-//        val intent = Intent(requireContext(), WelcomeActivity::class.java)
-//        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-//        startActivity(intent)
-//    }
+                withContext(Dispatchers.Main) {
+                    // Navigasi ke WelcomeActivity di Main Thread
+                    val intent = Intent(requireContext(), WelcomeActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
+                    Log.d("Logout", "Proses logout selesai. Pengguna diarahkan ke WelcomeActivity.")
+                }
+            } catch (e: Exception) {
+                Log.e("Logout", "Terjadi kesalahan saat logout: ${e.message}")
+            }
+        }
+    }
+
+    companion object {
+        const val databaseURL = BuildConfig.DATABASE_URL
+    }
 }
--- app/src/main/java/com/example/capstonebangkitpawers/register/RegisterActivity.kt
+++ app/src/main/java/com/example/capstonebangkitpawers/register/RegisterActivity.kt
@@ -21,11 +21,14 @@ import androidx.credentials.GetCredentialResponse
 import androidx.credentials.exceptions.GetCredentialException
 import androidx.lifecycle.lifecycleScope
 import com.example.capstonebangkitpawers.BuildConfig
+import com.example.capstonebangkitpawers.R
 import com.example.capstonebangkitpawers.databinding.ActivityRegisterBinding
 import com.example.capstonebangkitpawers.login.LoginActivity
 import com.example.capstonebangkitpawers.main.MainActivity
+import com.example.capstonebangkitpawers.view.VerifyEmailActivity
 import com.google.android.libraries.identity.googleid.GetGoogleIdOption
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.coroutines.launch
@@ -40,46 +43,55 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setupClickableText()
+        addTextWatchers()
         setupFirebaseAuth()
         setupListeners()
+        setupClickableText()
     }
 
     private fun setupFirebaseAuth() {
         auth = FirebaseAuth.getInstance()
     }
 
-    private fun setupClickableText() {
-        val spannableString = SpannableString("Sudah punya akun? Masuk")
-        val clickableSpan = object : ClickableSpan() {
-            override fun onClick(widget: View) {
-                startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
-            }
-        }
-
-        spannableString.setSpan(clickableSpan, 18, 23, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-        spannableString.setSpan(
-            ForegroundColorSpan(getColor(android.R.color.holo_blue_light)),
-            18, 23, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
-
-        binding.tvMasuk.text = spannableString
-        binding.tvMasuk.movementMethod = LinkMovementMethod.getInstance()
-    }
-
     private fun setupListeners() {
-        binding.btnGoogle.setOnClickListener { regisrerGoogle() }
         binding.btnDaftar.setOnClickListener { registerEmail() }
-        addTextWatchers()
+        //binding.btnGoogle.setOnClickListener { registerGoogle()}
     }
+
+//    private fun registerGoogle() {
+//        val credentialManager = CredentialManager.create(this)
+//
+//        val googleIdOption = GetGoogleIdOption.Builder()
+//            .setFilterByAuthorizedAccounts(false)
+//            .setServerClientId(webClientId)
+//            .build()
+//
+//        val request = GetCredentialRequest.Builder()
+//            .addCredentialOption(googleIdOption)
+//            .build()
+//
+//        lifecycleScope.launch {
+//            try {
+//                val result: GetCredentialResponse = credentialManager.getCredential(
+//                    request = request,
+//                    context = this@RegisterActivity,
+//                )
+//            } catch (e: GetCredentialException) {
+//                Log.d("Error", e.message.toString())
+//            }
+//        }
+//    }
 
     private fun registerEmail() {
         val email = binding.emailEditText.text.toString().trim()
         val name = binding.userEditText.text.toString().trim()
         val password = binding.pwEditText.text.toString().trim()
-        val confirmPassword = binding.confirmpwEditText.text.toString().trim()
 
-        if (!validateInput(email, name, password, confirmPassword)) return
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            showAlertDialog("Error", "Format email tidak valid.")
+        }
+
+        validatePasswords()
 
         binding.progressBar.visibility = View.VISIBLE
         auth.createUserWithEmailAndPassword(email, password)
@@ -87,9 +99,9 @@ class RegisterActivity : AppCompatActivity() {
                 binding.progressBar.visibility = View.GONE
                 if (task.isSuccessful) {
                     val user = auth.currentUser
-                    user?.uid?.let { saveUserDataToDatabase(it, name, email) }
-                    showAlertDialog("Pendaftaran Berhasil", "Akun Anda berhasil dibuat.") {
-                        navigateToLogin()
+                    if (user != null) {
+                        saveUserToDatabase(user.uid, name, email)
+                        sendVerificationEmail(user)
                     }
                 } else {
                     showAlertDialog("Pendaftaran Gagal", task.exception?.message ?: "Terjadi kesalahan.")
@@ -97,67 +109,134 @@ class RegisterActivity : AppCompatActivity() {
             }
     }
 
-    private fun saveUserDataToDatabase(uid: String, name: String, email: String) {
-        val userData = mapOf("name" to name, "email" to email)
-        FirebaseDatabase.getInstance().getReference("users").child(uid)
-            .setValue(userData)
-            .addOnSuccessListener { Log.d(TAG, "Data pengguna berhasil disimpan.") }
-            .addOnFailureListener { e -> Log.e(TAG, "Gagal menyimpan data: ${e.message}") }
+    private fun saveUserToDatabase(uid: String, name: String, email: String) {
+        val database = FirebaseDatabase.getInstance(databaseURL)
+        val userRef = database.getReference("users").child(uid)
+
+        val userData = mapOf(
+            "name" to name,
+            "email" to email,
+            "isVerified" to false,
+            "photoUrl" to "default",
+            "isLogin" to false
+        )
+
+        userRef.setValue(userData)
+            .addOnSuccessListener {
+                Log.d("RegisterActivity", "Data pengguna berhasil disimpan: $userData")
+            }
+            .addOnFailureListener { e ->
+                Log.e("RegisterActivity", "Gagal menyimpan data pengguna: ${e.message}")
+            }
     }
 
-    private fun validateInput(email: String, name: String, password: String, confirmPassword: String): Boolean {
-        if (name.isEmpty()) {
-            showAlertDialog("Error", "Nama tidak boleh kosong.")
-            return false
-        }
-        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            showAlertDialog("Error", "Format email tidak valid.")
-            return false
-        }
-        if (password.length < 8) {
-            binding.pwEditTextLayout.error = "Password harus terdiri dari minimal 8 karakter."
-            return false
-        }
-        if (password != confirmPassword) {
-            binding.confirmpwEditTextLayout.error = "Konfirmasi password tidak cocok."
-            return false
-        }
-        return true
+    private fun sendVerificationEmail(user: FirebaseUser) {
+        user.sendEmailVerification()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    navigateToVerifyEmail(user.email ?: "")
+                } else {
+                    showAlertDialog("Error", "Gagal mengirim email verifikasi.")
+                }
+            }
     }
 
-    private fun showAlertDialog(title: String, message: String, onPositiveButtonClick: (() -> Unit)? = null) {
+    private fun navigateToVerifyEmail(email: String) {
+        val intent = Intent(this, VerifyEmailActivity::class.java)
+        intent.putExtra("userEmail", email)
+        startActivity(intent)
+        finish()
+    }
+
+
+    private fun showAlertDialog(title: String, message: String) {
         AlertDialog.Builder(this)
             .setTitle(title)
             .setMessage(message)
-            .setCancelable(false)
-            .setPositiveButton("OK") { dialog, _ ->
-                dialog.dismiss()
-                onPositiveButtonClick?.invoke()
-            }
+            .setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
             .show()
     }
 
-    private fun regisrerGoogle() {
-        val credentialManager = CredentialManager.create(this)
-
-        val googleIdOption = GetGoogleIdOption.Builder()
-            .setFilterByAuthorizedAccounts(false)
-            .setServerClientId(webClientId)
-            .build()
-
-        val request = GetCredentialRequest.Builder()
-            .addCredentialOption(googleIdOption)
-            .build()
-
-        lifecycleScope.launch {
-            try {
-                val result: GetCredentialResponse = credentialManager.getCredential(
-                    request = request,
-                    context = this@RegisterActivity,
-                )
-            } catch (e: GetCredentialException) {
-                Log.d("Error", e.message.toString())
+    private fun setupClickableText() {
+        val spannableString = SpannableString("Sudah punya akun? Masuk")
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                navigateToLogin()
             }
+        }
+        spannableString.setSpan(clickableSpan, 18, 23, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableString.setSpan(
+            ForegroundColorSpan(getColor(android.R.color.holo_blue_light)),
+            18, 23, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        binding.tvMasuk.text = spannableString
+        binding.tvMasuk.movementMethod = LinkMovementMethod.getInstance()
+    }
+
+    private fun addTextWatchers() {
+        binding.emailEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!Patterns.EMAIL_ADDRESS.matcher(s.toString()).matches()) {
+                    binding.emailEditTextLayout.error = "Format email tidak valid"
+                } else {
+                    binding.emailEditTextLayout.error = null
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        binding.userEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.toString().isEmpty()) {
+                    binding.userEditTextLayout.error = "Nama tidak boleh kosong"
+                } else {
+                    binding.userEditTextLayout.error = null
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        binding.pwEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                validatePasswords()
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        binding.confirmpwEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                validatePasswords()
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
+
+    private fun validatePasswords() {
+        val password = binding.pwEditText.text.toString().trim()
+        val confirmPassword = binding.confirmpwEditText.text.toString().trim()
+
+        if (password.length < 8) {
+            binding.pwEditTextLayout.error = "Kata sandi harus terdiri dari setidaknya 8 karakter."
+        } else {
+            binding.pwEditTextLayout.error = null
+        }
+
+        if (confirmPassword.isNotEmpty() && password != confirmPassword) {
+            binding.confirmpwEditTextLayout.error = "Passwords tidak sama"
+        } else {
+            binding.confirmpwEditTextLayout.error = null
         }
     }
 
@@ -166,34 +245,9 @@ class RegisterActivity : AppCompatActivity() {
         finish()
     }
 
-    private fun addTextWatchers() {
-        binding.pwEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                validatePasswords()
-            }
-            override fun afterTextChanged(s: Editable?) {}
-        })
-
-        binding.confirmpwEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                validatePasswords()
-            }
-            override fun afterTextChanged(s: Editable?) {}
-        })
-    }
-
-    private fun validatePasswords() {
-        val password = binding.pwEditText.text.toString()
-        val confirmPassword = binding.confirmpwEditText.text.toString()
-
-        binding.pwEditTextLayout.error = if (password.length < 8) "Password minimal 8 karakter" else null
-        binding.confirmpwEditTextLayout.error = if (password != confirmPassword) "Password tidak cocok" else null
-    }
-
     companion object {
-        private const val TAG = "RegisterActivity"
         const val webClientId = BuildConfig.WEB_CLIENT_ID
+        const val databaseURL = BuildConfig.DATABASE_URL
     }
 }
+
